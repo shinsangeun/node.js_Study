7655ec4841f04434e75ba04f9b6d53dfc7bd5e4d
sangeun-shin <sse@pntbiz.com>
2019-02-12 00:17:08 +0900

    Add ajax function

--- 20190207/app.js
+++ 20190207/app.js
@@ -2,7 +2,7 @@ var express= require('express');
 var app = express();
 var router = require('./router/index');
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 
 app.listen(3000, function () {
     console.log("start, express server on port 3000");
@@ -13,9 +13,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(router);
+app.use(cors());
+
+
 
 app.post('/email_post',function (req, res){
     res.send("<h1> Post response "+ req.body.email+"  </h1>");
     console.log(req.body.email);
    // res.render('form.html', {'email':req.body.email})
 });
+
+
+app.post('/ajax_send_email', function (req, res) {
+    console.log(req.body.email);
+    //check validation
+    var responseData = {'result' : 'ok', 'email' : req.body.email};
+    res.json(responseData);
+})
