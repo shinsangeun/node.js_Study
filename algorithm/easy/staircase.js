e49d04508d87ae8f6b16d5fd0274da1df8275400
sangeun-shin <sse@pntbiz.com>
2019-02-15 00:41:15 +0900

    Add algorithm

---
+++ algorithm/easy/staircase.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the staircase function below.
+function staircase(n) {
+    for (var i = 1; i <= n; i++) {
+        var col = i;
+        for (var j = 1; j <= n - col; j++) {
+            process.stdout.write(' ');
+        }
+        for (var j = 1; j <= col; j++) {
+            process.stdout.write('#');
+        }
+        process.stdout.write('\n');
+    }
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    staircase(n);
+}
