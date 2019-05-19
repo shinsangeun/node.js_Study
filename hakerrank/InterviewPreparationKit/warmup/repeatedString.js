4cf6291947695adda0e9389c4bd5e37bfeaedade
shinsangeun <s0813se@naver.com>
2019-05-20 00:16:44 +0900

    test

--- hakerrank/InterviewPreparationKit/warmup/repeatedString.js
+++ hakerrank/InterviewPreparationKit/warmup/repeatedString.js
@@ -1,5 +1,17 @@
 // Complete the repeatedString function below.
 function repeatedString(s, n) {
+  //  var s ='aba';
+    var result = s.length * 3;
+
+    console.log(result);
+
+    if (result % s ==0) {
+        console.log(result*2);
+        return n*2;
+    } else {
+        console.log(result*2+1);
+        return n*2+1;
+    }
 
 }
 
