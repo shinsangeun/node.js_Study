936576f8ac2e524ba21c12545451398ad31a22ec
shinsangeun <s0813se@naver.com>
2019-05-23 00:11:08 +0900

    Add algorithm_picking numbers

--- hakerrank/InterviewPreparationKit/warmup/repeatedString.js
+++ hakerrank/InterviewPreparationKit/warmup/repeatedString.js
@@ -1,18 +1,49 @@
-// Complete the repeatedString function below.
-function repeatedString(s, n) {
-  //  var s ='aba';
-    var result = s.length * 3;
+'use strict';
 
-    console.log(result);
+const fs = require('fs');
 
-    if (result % s ==0) {
-        console.log(result*2);
-        return n*2;
-    } else {
-        console.log(result*2+1);
-        return n*2+1;
-    }
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
 
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
 }
 
-repeatedString('aba',10);
+function repeatedString(s, n) {
+    const counta = (str) => str.split('')
+        .filter(c => c == 'a')
+        .length;
+
+    const d = Math.floor(n / s.length);
+    const r = n - (s.length * d);
+    return d * counta(s) + counta(s.substr(0, r))
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const n = parseInt(readLine(), 10);
+
+    let result = repeatedString(s, n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
---
+++ hakerrank/algorithms/easy/pickingNumbers.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function pickingNumbers(a) {
+    var map = new Map();
+    var maxCount = 0;
+    for (var i = 0; i < a.length; i++) {
+        var count = 0;
+        if (!map.has(a[i])) {
+            map.set(a[i]);
+            for (var j = 0; j < a.length; j++) {
+                if (a[j] == a[i] || a[j] == a[i] + 1) {
+                    count++;
+                }
+            }
+            maxCount = Math.max(maxCount, count);
+        }
+    }
+    return maxCount;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = pickingNumbers(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
