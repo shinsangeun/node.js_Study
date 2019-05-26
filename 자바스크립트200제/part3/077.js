697f4e0e50c6adca05bc2797e7f6fda32ef9761f
shinsangeun <s0813se@naver.com>
2019-05-26 23:55:29 +0900

    Add javascript example_part3 Fix error hakerrank algorithms

---
+++ 자바스크립트200제/part3/077.js
@@ -0,0 +1,7 @@
+//includes
+//문자열.includes(문자열, 인덱스);
+const str = 'Make your lives extraordinary';
+
+console.log(str.includes('Make'));
+console.log(str.includes('Make',2));
+console.log(str.includes('extraordinary'));
