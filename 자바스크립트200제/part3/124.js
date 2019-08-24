3ec13c47dc4e7d8291d7805d9c35ede7965a5b65
shinsangeun <s0813se@naver.com>
2019-08-25 00:04:02 +0900

    Add javascript example_part3: 문자열/배열 순환

---
+++ 자바스크립트200제/part3/124.js
@@ -0,0 +1,14 @@
+//문자열 순환
+const str = "hello";
+
+for(const item of str){
+    console.log(item);
+}
+
+const iter = str[Symbol.iterator]();
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+
