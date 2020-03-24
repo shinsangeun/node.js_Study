8728e4817e447604c22ce6371117b5f869c3ac87
shinsangeun <s0813se@naver.com>
2020-03-24 23:55:03 +0900

    Add hackerRank algorithms -migratory birds

--- hackerRank/algorithms/Implementation/migratoryBirds.js
+++ hackerRank/algorithms/Implementation/migratoryBirds.js
@@ -1,31 +1,33 @@
 function migratoryBirds(arr) {
-    let number = [];
+    let map = {}, maxCount = 0, output = [];
+    for (let i = 0; i < arr.length; i++) {
+        console.log("1==>", map[arr[i]]);
 
-    arr.sort(function(a,b) {
-        return a-b;
-    });
-
-    for(let i = 0; i < arr.length;i++){
-        if(arr[i] == arr[i+1]){
-            number.push(arr[i]);
-            if((number.length ==2) && (number[0] == number[1])){
-                return Math.min(...number);
-            }
-        }else{
-            continue;
+        if (!map[arr[i]]) {
+            map[arr[i]] = 1;
+        } else {
+            map[arr[i]] += 1;
         }
 
+        // getting max count
+        if (map[arr[i]] > maxCount) {
+            console.log("2==>", map[arr[i]], maxCount);
+            maxCount = map[arr[i]];
+        }
     }
 
-    let test = [];
-    number.find(function (element) {
-        if(number[element] == number[element +1]){
-            test.push(number[element]);
-        }else{
-            return;
+
+    for (let j = 0; j < arr.length; j++) {
+        console.log("3==>", map[arr[j]], maxCount);
+        if (map[arr[j]] === maxCount) {
+            output.push(arr[j]);
+            console.log("4==>", output);
         }
-    });
-    return test[0];
+    }
+
+    console.log("5==>", output);
+    return Math.min(...output);
 }
 
-migratoryBirds([1, 4,4,4,5,3]);
+//migratoryBirds([ 1,2 ,2, 5, 2 ,5 ,5 ,5 ,2 ,5 ,5 ,5 ,5 ,2 ,5 ,1 ,5 ,2 ,5 ,5 ,2 ,2 ,5 ,5 ,2 ,5 ,2 ,5 ,5 ,2 ,2 ,2 ,2 ,2 ,2 ,5,2 ,1 ,2 ,2, 5, 2 ,5 ,5 ,5 ,2 ,5 ,5 ,5 ,5 ,2 ,5 ,1 ,5 ,2 ,5 ,5 ,2 ,2 ,5 ,5 ,2 ,5 ,2 ,5 ,5 ,2 ,2 ,2 ,2 ,2 ,2 ,5,2,1, 2 ,2, 5, 2 ,5 ,5 ,5 ,2 ,5 ,5 ,5 ,5 ,2 ,5 ,1 ,5 ,2 ,5 ,5 ,2 ,2 ,5 ,5 ,2 ,5 ,2 ,5 ,5 ,2 ,2 ,2 ,2 ,2 ,2 ,5,2 ]);
+migratoryBirds([1,4,4,4,5,3 ]);
